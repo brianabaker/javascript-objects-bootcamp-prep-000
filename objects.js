7157d2f14b6f60480182a470d47696876dc67121
Briana Baker <brianaclairbaker@gmail.com>
2017-11-13 19:40:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,5 @@
 var playlist = {aristName: 'songTitle'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle})
-}
-
-function removeFromPlaylist(playlist, artistName){
-  return delete playlist.artistName;
+  
 }
