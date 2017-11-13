86a942df7c69678ddaf8a36f6adacb6efddcba72
Briana Baker <brianaclairbaker@gmail.com>
2017-11-13 19:42:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {aristName: 'songTitle'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign(playlist, {artistName: songTitle})
+  Object.assign(playlist, {[artistName]: songTitle})
   return playlist;
 }
